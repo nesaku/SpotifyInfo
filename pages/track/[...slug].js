b260d38e83559bed011fce9b32f7ae5eabe92423
Nesaku <78386413+nesaku@users.noreply.github.com>
2023-11-11 16:32:18 -0500

    replace <a> tag with <Link />

--- pages/track/[...slug].js
+++ pages/track/[...slug].js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Loader from "../../components/Loader";
@@ -57,13 +58,8 @@ const Slug = () => {
                 https://open.spotify.com/track/3CRDbSIZ4r5MsZ0YwxuEkn
               </p>
             </h2>
-            <div className="mx-auto">
-              <a
-                href="/"
-                className="font-semibold text-md text-gray-900 dark:text-white bg-green-500 ring ring-green-600 ring-offset-2 ring-offset-green-100 py-5 px-2 rounded-xl shadow-lg shadow-green-500 hover:shadow-xl hover:bg-green-600 transition duration-300 delay-40 hover:delay-40"
-              >
-                Go Back Home
-              </a>
+            <div className=" mx-auto font-semibold text-md text-gray-900 dark:text-white bg-green-500 ring ring-green-600 ring-offset-2 ring-offset-green-100 py-5 px-2 rounded-xl shadow-lg shadow-green-500 hover:shadow-xl hover:bg-green-600 transition duration-300 delay-40 hover:delay-40">
+              <Link href="/">Go Back Home</Link>
             </div>
           </div>
         )}
